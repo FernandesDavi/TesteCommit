c87ac85b4c7580f90e86095f4c59192ca85dd999
Davi Fernandes <t_davif1@verifone.com>
2017-03-16 13:25:00 -0300

    teste

--- AreaController.cs
+++ AreaController.cs
@@ -22,35 +22,7 @@ namespace Verifone.MES.Site.Controllers
             _localizer = localizer;
         }
 
-		// GET: /<controller>/
-        #region GET
-        [HttpGet]
-        public IActionResult Index2()
-        {
-            List<ViewAreaFilter> view = new List<ViewAreaFilter>();
-            ViewBag.AreaType = GetAllAreaType().Select(x => new SelectListItem { Text = x.Name, Value = x.AreaTypeId.ToString() }).ToArray();
-            ViewBag.SerchArea = new ViewAreaFilter();
-
-            string componentSearch = HttpContext.Session.GetString("AreaSearch");
-            string boolSearch = HttpContext.Session.GetString("AreaBoolSearch");
-
-            if (!String.IsNullOrEmpty(boolSearch) && !String.IsNullOrEmpty(componentSearch))
-            {
-                view = JsonConvert.DeserializeObject<List<ViewAreaFilter>>(componentSearch);
-            }
-
-            HttpContext.Session.SetString("AreaBoolSearch", "");
-            CheckForAlerts();
-            return View(view);
-        }
-		
-        // GET: /<controller>/
-        #region GET
-        [HttpGet]
-        public string teste(){
-            List<string> teste = new List<string>();
-        }
-        public IActionResult Index()
+		public IActionResult Index()
         {
             List<ViewAreaFilter> view = new List<ViewAreaFilter>();
             ViewBag.AreaType = GetAllAreaType().Select(x => new SelectListItem { Text = x.Name, Value = x.AreaTypeId.ToString() }).ToArray();
